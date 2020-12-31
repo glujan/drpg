987b93f0b9f1df639b0911d908e1daabf0abd79e
Grzegorz Janik <3079032+glujan@users.noreply.github.com>
2020-12-31 14:09:35 +0100

    Create python package using setuptools

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.in", "r", encoding="utf-8") as fh:
+    install_requires = fh.read().splitlines()
+
+setuptools.setup(
+    name="drpg",
+    version="0.0.6",
+    author="Grzegorz Janik",
+    description="Download and keep up to date your purchases from DriveThruRPG",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/glujan/drpg",
+    packages=["drpg"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": ["drpg=drpg.cmd:run"],
+    },
+    python_requires=">=3.8",
+    install_requires=install_requires,
+)

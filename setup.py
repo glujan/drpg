783d3893c2c9e2b127ce7d73a2e434d66553daf1
Grzegorz Janik <3079032+glujan@users.noreply.github.com>
2023-06-12 07:15:12 +0200

    Move coverage config to pyproject.toml

--- setup.py
+++
@@ -1,8 +0,0 @@
-import setuptools
-
-if __name__ == "__main__":
-    # setuptools does not support setting install_requires from a file in setup.cfg
-    with open("requirements.in", encoding="utf-8") as fh:
-        install_requires = fh.read().splitlines()
-
-    setuptools.setup(install_requires=install_requires)

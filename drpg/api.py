540abc078010ab910d3bf864a3abdaed4deb7c08
Grzegorz Janik <3079032+glujan@users.noreply.github.com>
2024-04-11 22:23:53 +0200

    Fetch chosen fields when listing products

--- drpg/api.py
+++ drpg/api.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 
 import logging
+from enum import Enum
 from time import sleep
 from typing import TYPE_CHECKING
 
 import httpx
 
 if TYPE_CHECKING:  # pragma: no cover
-    from typing import Iterator, TypedDict
+    from typing import Iterable, Iterator, TypedDict
 
     class TokenResponse(TypedDict):
         customers_id: str
@@ -35,6 +36,16 @@ if TYPE_CHECKING:  # pragma: no cover
 
 
 logger = logging.getLogger("drpg")
+
+
+class ProductField(str, Enum):
+    products_name = "products_name"
+    cover_url = "cover_url"
+    date_purchased = "date_purchased"
+    products_filesize = "products_filesize"
+    publishers_name = "publishers_name"
+    products_thumbnail100 = "products_thumbnail100"
+    is_archived = "is_archived"
 
 
 class DrpgApi:
@@ -71,12 +82,16 @@ class DrpgApi:
         self._client.headers["Authorization"] = f"Bearer {login_data['access_token']}"
         return login_data
 
-    def customer_products(self, per_page: int = 100) -> Iterator[Product]:
+    def customer_products(
+        self,
+        per_page: int = 1,
+        fields: Iterable[ProductField] = (ProductField.publishers_name, ProductField.products_name),
+    ) -> Iterator[Product]:
         """List all not archived customer's products."""
 
         page = 1
 
-        while result := self._product_page(page, per_page):
+        while result := self._product_page(page, per_page, fields):
             logger.debug("Yielding products page %d", page)
             yield from result
             page += 1
@@ -128,13 +143,15 @@ class DrpgApi:
         logger.debug("Got download link for: %s - %s", product_id, item_id)
         return data
 
-    def _product_page(self, page: int, per_page: int) -> list[Product]:
+    def _product_page(
+        self, page: int, per_page: int, fields: Iterable[ProductField]
+    ) -> list[Product]:
         """
         List products from a specified page.
 
         Available "fields" values:
             products_name, cover_url, date_purchased, products_filesize,
-            publishers_name, products_thumbnail100
+            publishers_name, products_thumbnail100, is_archived
         Available "embed" values:
             files.filename, files.last_modified, files.checksums, files.raw_filesize,
             filters.filters_name, filters.filters_id, filters.parent_id
@@ -147,7 +164,7 @@ class DrpgApi:
                 "page": page,
                 "per_page": per_page,
                 "include_archived": 0,
-                "fields": "publishers_name,products_name",
+                "fields": "".join(field.value for field in fields),
                 "embed": "files.filename,files.last_modified,files.checksums",
             },
         ).json()["message"]
